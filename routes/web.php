4fd5ab8c8dfa76bf6db6b0ef1ebdac2b9bfc7b60
Talha Imran <talhaimran284@gmail.com>
2021-01-07 22:09:36 +0500

    Returning different view depending upon the URL

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
